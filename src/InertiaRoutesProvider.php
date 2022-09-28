03e2923d7a5ccba779bb966e3a5877c95e702962
Craig Riley <c.riley@dsmdesign.co.uk>
2022-09-28 08:18:18 +0100

    A failed attempt...

--- src/InertiaRoutesProvider.php
+++ src/InertiaRoutesProvider.php
@@ -11,16 +11,38 @@ class InertiaRoutesProvider extends ServiceProvider
 {
     public function register()
     {
+        $this->app->bind(\Inertia\Ssr\HttpGateway::class, \AdminUI\InertiaRoutes\TidyHttpGateway::class);
         //
     }
 
     public function boot(Request $request)
     {
-        $this->app->bind(\Inertia\Ssr\HttpGateway::class, \AdminUI\InertiaRoutes\TidyHttpGateway::class);
+        $firstLoadOnlyProps = $request->inertia() ? null : function () use ($request) {
+            $group = config('inertia.route_group', null);
+            $only = config('inertia.route_only', null);
+            $except = config('inertia.route_except', null);
 
-        $group = config('inertia.route_group', null);
+            $initialOnly = config('ziggy.only');
+            $initialExcept = config('ziggy.except');
 
-        $firstLoadOnlyProps = $request->inertia() ? null : fn () => new Ziggy($group, $request->url());
+            if (!empty($only)) {
+                config(['ziggy.only' => $only]);
+            }
+            if (!empty($except)) {
+                config(['ziggy.except' => $except]);
+            }
+
+            $routes = new Ziggy($group, $request->url());
+            $jsonRoutes = $routes->toArray();
+
+            if (!empty($only)) {
+                config(['ziggy.only' => $initialOnly]);
+            }
+            if (!empty($except)) {
+                config(['ziggy.except' => $initialExcept]);
+            }
+            return $jsonRoutes;
+        };
         Inertia::share('ziggy', $firstLoadOnlyProps);
     }
 }
