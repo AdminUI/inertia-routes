a2db4ce17a5960b53e8f71765e68660d4511bfe3
Craig Riley <c.riley@dsmdesign.co.uk>
2024-03-15 06:07:03 +0000

    Remove console calls from dist files #patch

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+	esbuild: {
+		pure: mode === "production" ? ["console.log"] : [],
+	},
 	plugins: [vue(), visualizer()],
 	resolve: {
 		alias: {
@@ -20,4 +23,4 @@ export default defineConfig({
 			external: ["vue", "@inertiajs/vue3"],
 		},
 	},
-});
+}));
